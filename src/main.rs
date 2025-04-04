1801f86b2a7b9493ca87be64f0d19d80e5acaa21
YoonSoo Kim <yoonskim37@gmail.com>
2025-04-04 09:53:47 +0900

    feat: CLI 지원 기능 추가 (main) 및 빌드를 위한 toml bin 추가

--- src/main.rs
+++ src/main.rs
@@ -8,31 +8,46 @@ use lexer::Lexer;
 use parser::{Parser, Command};
 use interpreter::Interpreter;
 
+use std::env;
 use std::fs;
 
-fn main() {
-    // ✅ 1. DSL 스크립트 파일 읽기
-    let source_path = "test/test_script.jdl";
+// ✅ 디버그 출력용 플래그
+const DEBUG: bool = false;
 
+fn main() {
+    // ✅ 명령줄 인자 처리
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("❌ Usage: mydsl <script.jdl>");
+        std::process::exit(1);
+    }
+
+    let source_path = &args[1];
+
+    // ✅ 1. DSL 스크립트 파일 읽기
     let source = fs::read_to_string(source_path)
         .unwrap_or_else(|e| {
             eprintln!("❌ Failed to read DSL file '{}': {}", source_path, e);
             std::process::exit(1);
         });
 
-    println!("🔹 DSL Script Loaded From '{}':\n", source_path);
-    println!("{}", source);
-    println!();
+    if DEBUG {
+        println!("🔹 DSL Script Loaded From '{}':\n", source_path);
+        println!("{}", source);
+        println!();
+    }
 
     // ✅ 2. 렉서 실행 → 입력 문자열을 Token 리스트로 변환
     let mut lexer = Lexer::new(&source);
     let tokens = lexer.tokenize();
 
-    println!("🔹 Tokens:");
-    for (i, token) in tokens.iter().enumerate() {
-        println!("  [{:02}] {:?}", i, token);
+    if DEBUG {
+        println!("🔹 Tokens:");
+        for (i, token) in tokens.iter().enumerate() {
+            println!("  [{:02}] {:?}", i, token);
+        }
+        println!();
     }
-    println!();
 
     // ✅ 3. 파서 실행 → Token 리스트를 Command 리스트(AST)로 변환
     let mut parser = Parser::new(tokens);
@@ -44,14 +59,18 @@ fn main() {
         }
     };
 
-    println!("🔹 Parsed Commands:");
-    for (i, cmd) in commands.iter().enumerate() {
-        println!("  [{:02}] {:?}", i, cmd);
+    if DEBUG {
+        println!("🔹 Parsed Commands:");
+        for (i, cmd) in commands.iter().enumerate() {
+            println!("  [{:02}] {:?}", i, cmd);
+        }
+        println!();
     }
-    println!();
 
     // ✅ 4. 인터프리터 실행 → 명령어(Command) 리스트를 실제 동작으로 실행
-    println!("🔹 Interpreter Output:");
+    if DEBUG {
+       println!("🔹 Interpreter Output:");
+    }
     let mut interpreter = Interpreter::new();
 
     if let Err(e) = interpreter.run(commands) {
